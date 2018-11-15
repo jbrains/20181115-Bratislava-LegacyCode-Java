c1ffcd28a39cb04b9ee688b81f556649a842b201
J. B. Rainsberger <me@jbrains.ca>
2018-11-15 13:52:40 +0100

    We can now more-easily test rolling dice and moving the player around the board.

--- src/test/java/ca/jbrains/trivia/test/RollDiceTest.java
+++ src/test/java/ca/jbrains/trivia/test/RollDiceTest.java
@@ -5,33 +5,33 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class RollDiceTest {
-    @Test
-    public void happyPath() throws Exception {
-        class InspectableGame extends Game {
-            public InspectableGame(int startingPlace) {
-                add("::irrelevant player name::");
-                putPlayerInPlace(indexOfTheOnlyPlayer(), startingPlace);
-            }
-
-            // REFACTOR Move to some kind of SetupGame module
-            public void putPlayerInPlace(int playerIndex, int place) {
-                places[playerIndex] = place;
-            }
-
-            public int placeForPlayer(int playerIndex) {
-                return places[playerIndex];
-            }
-
-            // REFACTOR I wouldn't need this if I could inspect a Turn.
-            private int indexOfLastPlayerWhoPlayed() {
-                return indexOfTheOnlyPlayer();
-            }
-
-            private int indexOfTheOnlyPlayer() {
-                return 0;
-            }
+    public static class InspectableGame extends Game {
+        public InspectableGame(int startingPlace) {
+            add("::irrelevant player name::");
+            putPlayerInPlace(indexOfTheOnlyPlayer(), startingPlace);
         }
 
+        // REFACTOR Move to some kind of SetupGame module
+        public void putPlayerInPlace(int playerIndex, int place) {
+            places[playerIndex] = place;
+        }
+
+        public int placeForPlayer(int playerIndex) {
+            return places[playerIndex];
+        }
+
+        // REFACTOR I wouldn't need this if I could inspect a Turn.
+        private int indexOfLastPlayerWhoPlayed() {
+            return indexOfTheOnlyPlayer();
+        }
+
+        private int indexOfTheOnlyPlayer() {
+            return 0;
+        }
+    }
+
+    @Test
+    public void happyPath() throws Exception {
         InspectableGame game = new InspectableGame(0);
         game.roll(3);
         Assert.assertEquals(3, game.placeForPlayer(game.indexOfLastPlayerWhoPlayed()));
