9cf1d2f25fd04bc63a0e3605d205fc5cf0ae1138
J. B. Rainsberger <me@jbrains.ca>
2018-11-15 15:38:42 +0100

    Identified a design problem related to rolling dice.

--- src/test/groovy/ca/jbrains/trivia/test/HowAPlayerMovesAroundTheBoard.groovy
+++ src/test/groovy/ca/jbrains/trivia/test/HowAPlayerMovesAroundTheBoard.groovy
@@ -13,5 +13,26 @@ class HowAPlayerMovesAroundTheBoard extends Specification {
         where:
         startingPlace | roll || endingPlace
         0             | 3    || 3
+        0             | 6    || 6
+        3             | 6    || 9
+        8             | 3    || 11
+        11            | 1    || 0
+        11            | 6    || 5
+    }
+
+    def "the board is not quite circular"() {
+        def game = new SilentAndInspectableSinglePlayerGame(startingPlace)
+
+        expect:
+        game.roll(roll)
+        game.placeForPlayer(game.indexOfLastPlayerWhoPlayed()) == endingPlace
+
+        // These tests will fail once we "fix" moving around the
+        // board to use % 12 in place of subtracting 12.
+        where:
+        startingPlace | roll || endingPlace
+        0             | 24   || 12
+        0             | 25   || 13
+        13            | 39   || 40
     }
 }
